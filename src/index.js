6d43dc3960604265855da4e312284500452de523
silent10z <dkgueing@gmail.com>
2023-02-01 23:48:42 +0900

    chore: jsdoc 초기 설정

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} title - 아이디
+ * @param {string} content - 내용
+ * @param {string} isComplete - 완료 여부
+ * @param {string} category - 카테고리
+ * @param {string} tags - 태그들
+ * @returns {object} - 오브젝트들
+ */
+const addTodo = (id, content, isComplete, category, tags) =>{}
